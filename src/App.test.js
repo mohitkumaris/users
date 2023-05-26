3899e701d2abe17deb61e700ab89396571b1afed
Mohit Kumar <mohitkumaris@gmail.com>
2023-05-26 14:45:15 +0530

    final commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,27 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("user can enter form data and will show on list", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  const nameInput = screen.getByRole("textbox", {
+    name: /name/i,
+  });
+  const emailInput = screen.getByRole("textbox", {
+    name: /email/i,
+  });
+
+  userEvent.click(nameInput);
+  userEvent.keyboard("mohit");
+
+  userEvent.click(emailInput);
+  userEvent.keyboard("mohit@gmail.com");
+
+  const button = screen.getByRole("button");
+  userEvent.click(button);
+
+  // to debug test for fields
+
+  screen.debug();
 });
